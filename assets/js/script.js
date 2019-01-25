0d904b5779a7dad62eb14ebf165eb20e49dd5ba1
Glatigny <f.west@live.fr>
2019-01-25 12:08:59 +0100

    js part 2 exo 6

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+function change(object, ev)
+{
+   if(ev == "over"){
+     object.src = object.src.replace(/image1(\..+)$/, 'image1_2$1');
+   }else{
+     object.src = object.src.replace(/image1_2(\..+)$/, 'image1$1');
+   }
+   if(ev == "over"){
+     object.src = object.src.replace(/image2(\..+)$/, 'image2_2$1');
+   }else{
+     object.src = object.src.replace(/image2_2(\..+)$/, 'image2$1');
+   }
+   if(ev == "over"){
+     object.src = object.src.replace(/image3(\..+)$/, 'image3_2$1');
+   }else{
+     object.src = object.src.replace(/image3_2(\..+)$/, 'image3$1');
+   }
+   if(ev == "over"){
+     object.src = object.src.replace(/image4(\..+)$/, 'image4_2$1');
+   }else{
+     object.src = object.src.replace(/image4_2(\..+)$/, 'image4$1');
+   }
+   if(ev == "over"){
+     object.src = object.src.replace(/image5(\..+)$/, 'image5_2$1');
+   }else{
+     object.src = object.src.replace(/image5_2(\..+)$/, 'image5$1');
+   }
+}
