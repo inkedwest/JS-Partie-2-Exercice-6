ec9bb1c901a6db6ec68c2feb2c89f17c95fc2516
Glatigny <f.west@live.fr>
2019-01-28 15:18:10 +0100

    correction exo 6

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,8 @@
-function change(object, ev)
-{
-   if(ev == "over"){
-     object.src = object.src.replace(/image1(\..+)$/, 'image1_2$1');
-   }else{
-     object.src = object.src.replace(/image1_2(\..+)$/, 'image1$1');
-   }
-   if(ev == "over"){
-     object.src = object.src.replace(/image2(\..+)$/, 'image2_2$1');
-   }else{
-     object.src = object.src.replace(/image2_2(\..+)$/, 'image2$1');
-   }
-   if(ev == "over"){
-     object.src = object.src.replace(/image3(\..+)$/, 'image3_2$1');
-   }else{
-     object.src = object.src.replace(/image3_2(\..+)$/, 'image3$1');
-   }
-   if(ev == "over"){
-     object.src = object.src.replace(/image4(\..+)$/, 'image4_2$1');
-   }else{
-     object.src = object.src.replace(/image4_2(\..+)$/, 'image4$1');
-   }
-   if(ev == "over"){
-     object.src = object.src.replace(/image5(\..+)$/, 'image5_2$1');
-   }else{
-     object.src = object.src.replace(/image5_2(\..+)$/, 'image5$1');
-   }
+// Ici nous allons chercher la deuxième image
+function changeImage(img){
+  document.getElementById(img).src ='assets/images/' + img + '_2.jpg';
+}
+// et ici nous allons récupérer la première image
+function resetImage(img){
+  document.getElementById(img).src ='assets/images/' + img + '.jpg';
 }
